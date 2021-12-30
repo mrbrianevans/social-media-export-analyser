5353f8d7704dd230610fccd2697da0ad3f194cad
Brian Evans <ebrian101@gmail.com>
2021-12-30 09:49:12 +0000

    Change Instagram posts to VirtualList to lazy load
    Signed-off-by: Brian Evans <ebrian101@gmail.com>
    
    #instagram

--- client/src/visualisations/specific/instagram/InstagramPostsList.svelte
+++ client/src/visualisations/specific/instagram/InstagramPostsList.svelte
@@ -1,21 +1,23 @@
 <script lang='ts'>
+  import VirtualList from '@sveltejs/svelte-virtual-list';
   import { Media } from '../../../../../lib/vendors/instagram/Media'
   import { longDateTime } from '../../../../../lib/common/DateUtils'
 
   export let data: Media
+  let start
+  let end
 </script>
 
 <div class='container'>
-  <h3 class='title'>Instagram posts</h3>
-  {#each data.photos as post}
-    <div>
+  <VirtualList items={data.photos} let:item={post} bind:start bind:end height="1000px">
+    <div class='post'>
       <p>
-        <b>{post.caption}</b>
+        <span>{post.caption}</span>
         <br />Posted on {longDateTime(new Date(post.taken_at))}
         <br />Path: {post.path}
       </p>
     </div>
-  {/each}
+  </VirtualList>
 </div>
 
 <style>
@@ -25,11 +27,7 @@
         margin: var(--lumo-space-l);
     }
 
-    .title {
-        margin: 0;
-    }
-
-    .container > div {
+    .container div.post {
       background-color: var(--lumo-contrast-10pct);
       border-color: var(--lumo-primary-color-50pct);
       border-width: medium;
@@ -37,5 +35,6 @@
       border-radius: var(--lumo-border-radius-l);
       padding: var(--lumo-space-m);
       margin: 0.5rem 1rem;
+      width: 600px;
     }
 </style>
